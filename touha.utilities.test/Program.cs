ae289a34a1ba6f4bc593aa546107e5d9127ccea1
tang bo <1129597198@qq.com>
2017-07-26 21:47:24 +0800

    no comment

--- touha.utilities.test/Program.cs
+++ touha.utilities.test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,51 @@ namespace touha.utilities.test
     {
         static void Main(string[] args)
         {
+            //Task<string> task = HttpHelper.DoPost("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm", new Dictionary<string, string>()
+            //{
+            //    {"tel", "15850781443"}
+            //});
+            //try
+            //{
+            //    Task<string> task = HttpHelper.DoPost(_address, new Dictionary<string, string>());
+            //    Console.Write(task.Result);
+                
+            //}catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.Read();
+
         }
 
         
+
+        static string _address = "http://api.worldbank.org/countries?format=json";
+
+        //static async void Run()
+        //{
+        //    // Create an HttpClient instance
+        //    HttpClient client = new HttpClient();
+
+        //    // Send a request asynchronously continue when complete
+        //    HttpResponseMessage response = await client.GetAsync(_address);
+
+        //    // Check that response was successful or throw exception
+        //    response.EnsureSuccessStatusCode();
+
+        //    // Read response asynchronously as JsonValue and write out top facts for each country
+        //    JsonArray content = await response.Content.ReadAsAsync<JsonArray>();
+
+        //    Console.WriteLine("First 50 countries listed by The World Bank...");
+        //    foreach (var country in content[1])
+        //    {
+        //        Console.WriteLine("   {0}, Country Code: {1}, Capital: {2}, Latitude: {3}, Longitude: {4}",
+        //            country.Value["name"],
+        //            country.Value["iso2Code"],
+        //            country.Value["capitalCity"],
+        //            country.Value["latitude"],
+        //            country.Value["longitude"]);
+        //    }
+        //}
     }
 }
--- touha.utilities/Http/HttpHelper.cs
+++ touha.utilities/Http/HttpHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace touha.utilities
 {
     public class HttpHelper
     {
+
         /// <summary>
         /// HttpClient实现Post请求
         ///  实例参数：
@@ -21,13 +23,16 @@ namespace touha.utilities
         /// <paramref name="parameters"></paramref>
         /// <paramref name="url"></paramref>
         /// </summary>
-        static async Task<string> DoPost(string url, Dictionary<string, string> parameters)
+        public static async Task<string> DoPost(string url, Dictionary<string, string> parameters)
         {
             //设置HttpClientHandler的AutomaticDecompression
             var handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip };
             //创建HttpClient（注意传入HttpClientHandler）
             using (var http = new HttpClient(handler))
             {
+                //sets the Accept header to "application/json"
+                http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
                 //使用FormUrlEncodedContent做HttpContent
                 var content = new FormUrlEncodedContent(parameters);
 
