8e8d5663a75bf8a0467292ff9ba3a0040cfca76f
tang bo <1129597198@qq.com>
2017-07-26 20:42:27 +0800

    first edition

---
+++ touha.utilities.test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace touha.utilities.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        
+    }
+}
---
+++ touha.utilities/Http/HttpHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace touha.utilities
+{
+    public class HttpHelper
+    {
+        /// <summary>
+        /// HttpClient实现Post请求
+        ///  实例参数：
+        ///  url:https://tcc.taobao.com/cc/json/mobile_tel_segment.htm
+        ///  parameters:new Dictionary<string, string>()
+        ///    {
+        ///        {"tel", "15850781443"}
+        ///     }
+        /// <paramref name="parameters"></paramref>
+        /// <paramref name="url"></paramref>
+        /// </summary>
+        static async Task<string> DoPost(string url, Dictionary<string, string> parameters)
+        {
+            //设置HttpClientHandler的AutomaticDecompression
+            var handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip };
+            //创建HttpClient（注意传入HttpClientHandler）
+            using (var http = new HttpClient(handler))
+            {
+                //使用FormUrlEncodedContent做HttpContent
+                var content = new FormUrlEncodedContent(parameters);
+
+                //await异步等待回应
+
+                var response = await http.PostAsync(url, content);
+                //确保HTTP成功状态值
+                response.EnsureSuccessStatusCode();
+                //await异步读取最后的JSON（注意此时gzip已经被自动解压缩了，因为上面的AutomaticDecompression = DecompressionMethods.GZip）
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
